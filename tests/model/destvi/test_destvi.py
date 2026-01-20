02a36612772a419c0a1ac195c09400e36d6ccf74
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2026-01-20 15:56:05 +0200

    updates

--- tests/model/destvi/test_destvi.py
+++ tests/model/destvi/test_destvi.py
@@ -60,7 +60,6 @@ def test_destvi():
             spatial_model.get_normalized_expression()
 
 
-@pytest.mark.internet
 def test_destvi_new(save_path: str):
     CELL_TYPE_ID = "broad_cell_types"
     G = 2000
