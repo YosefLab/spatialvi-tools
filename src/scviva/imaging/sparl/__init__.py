9bacbe3345ed72dd1dad3e2f65187568eeecd719
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2026-06-01 13:47:47 +0300

    updated html

--- src/scviva/imaging/sparl/__init__.py
+++ src/scviva/imaging/sparl/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
 from scviva.imaging.sparl._model import SPARL
+from scviva.imaging.sparl._module import SPARLModule
 
-__all__ = ["SPARL"]
+__all__ = ["SPARL", "SPARLModule"]
